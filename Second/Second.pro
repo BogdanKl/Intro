ae8187e241a64aad74ad4bbfe85a3ab617b2ac75
unknown <ogdan@list.ru>
2013-04-11 22:22:09 +0400

    ports

---
+++ Second/Second.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-03-15T14:19:11
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Second
+TEMPLATE = app
+SOURCES += main.cpp\
+        mainwindow.cpp
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+#CONFIG += extserialport
+include(3rdparty/qextserialport/src/qextserialport.pri)
---
+++ Second/mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <qextserialport.h>
+
+#include <QtGui/QMainWindow>
+#include <QTimer>
+#include <QDebug>
+#include <QByteArray>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    QextSerialPort *port;
+    QextSerialPort *portr;
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    QTimer *t;
+ signals:
+    void contin();
+
+private slots:
+    void on_Begin_clicked();
+    void iter();
+    void on_End_clicked();
+    void doing();
+
+    void on_Quit_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
